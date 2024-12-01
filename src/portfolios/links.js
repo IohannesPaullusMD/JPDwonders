c4468083d69011921169a66837ab0d54ae33619d
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2024-12-01 18:53:30 +0800

    fixed a bug in links.js

--- src/portfolios/links.js
+++ src/portfolios/links.js
@@ -6,12 +6,12 @@ class PortfolioLinks extends HTMLElement {
             <br>
             <div>
                 <ul>
-                    <li> <a href="./page.html"> Portfolio 1</a> </li>
-                    <li> <a href="./page.html"> Portfolio 2</a> </li>
-                    <li> <a href="./page.html"> Portfolio 3</a> </li>
-                    <li> <a href="./page.html"> Portfolio 4</a> </li>
-                    <li> <a href="./page.html"> Portfolio 5</a> </li>
-                    <li> <a href="./page.html"> Portfolio 6</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 1</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 2</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 3</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 4</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 5</a> </li>
+                    <li> <a href="../portfolios/page.html"> Portfolio 6</a> </li>
                 </ul>
             </div>
             <br>
