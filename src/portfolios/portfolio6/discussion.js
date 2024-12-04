561a4944a1184ebac88b28bc635949d1c04e43f4
IohannesPaullusMD <141614774+IohannesPaullusMD@users.noreply.github.com>
2024-12-04 08:46:26 +0800

    implemented page for portfolio 6

--- src/portfolios/portfolio6/discussion.js
+++ src/portfolios/portfolio6/discussion.js
@@ -1,16 +1,32 @@
 function fn() {
         const tab = "&nbsp;&nbsp;&nbsp;&nbsp;";
 
-        const discussionText = `${tab} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Proin tortor purus platea sit eu id nisi litora libero. Neque vulputate consequat ac amet augue blandit maximus aliquet congue.
-        Pharetra vestibulum posuere ornare faucibus fusce dictumst orci aenean eu facilisis ut volutpat commodo senectus purus himenaeos 
-        fames primis convallis nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Proin tortor purus platea sit eu id nisi litora libero. Neque vulputate consequat ac amet augue blandit maximus aliquet congue.
-        Pharetra vestibulum posuere ornare faucibus fusce dictumst orci aenean eu facilisis ut volutpat commodo senectus purus himenaeos 
-        fames primis convallis nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Proin tortor purus platea sit eu id nisi litora libero. Neque vulputate consequat ac amet augue blandit maximus aliquet congue.
-        Pharetra vestibulum posuere ornare faucibus fusce dictumst orci aenean eu facilisis ut volutpat commodo senectus purus himenaeos 
-        fames primis convallis nisi.
+        const discussionText = `${tab} Supercomputers are the giants of the computing world, designed for complex
+                        computations that require immense processing power. They are used in fields such as
+                        climate research, quantum mechanics, and space exploration. Mainframes, on the other
+                        hand, are powerful computers used primarily by large organizations for critical
+                        applications, large-scale transaction processing, and database management.
+                        <br> <br>
+                        ${tab} Mini Computers offer moderate processing speed and memory capacity, making
+                        them suitable for small to medium-sized applications within business environments.
+                        They consume less power than mainframes but more than microcomputers and are
+                        designed for multi-user tasks. On the other hand, microcomputers, commonly known as
+                        personal computers, have the lowest processing speed and memory capacity among
+                        the six types. They are energy-efficient, compact, and ideal for individual use, handling
+                        everyday tasks like word processing and web browsing.
+                        <br> <br>
+                        ${tab} Workstations boast high processing speeds and memory capacity, designed for
+                        professional tasks that require significant computational power, such as 3D rendering
+                        and scientific simulations. They consume more power than microcomputers due to their
+                        advanced hardware. Lastly, Servers, these are built for managing network resources
+                        and handling large databases, offering high processing speeds and substantial memory
+                        capacity. They have high power consumption to ensure reliability and continuous
+                        operation, varying in size from compact units to large systems.
+                        <br> <br>
+                        ${tab}  In conclusion, each type of computer serves specific roles, from the powerful
+                        supercomputers used in scientific research to the accessible microcomputers used in
+                        education and hobby projects. Understanding the characteristics and capabilities of
+                        each type helps in selecting the right computer for specific needs and applications.
         `;
 
         const discussionTag = document.getElementById('discussion');
